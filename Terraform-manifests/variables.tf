5965d31ff946b44025eac9559823ca9ea25d9742
Naveen <42107454+naveenatnew@users.noreply.github.com>
2021-12-01 14:33:37 +0530

    Add files via upload

---
+++ Terraform-manifests/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type = string
+  description = "Azure Region where all these resources will be provisioned"
+  default = "Central US"
+}
+
+variable "prefix" {
+  default = "Naveen"
+}
